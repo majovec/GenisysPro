5f0e7a7c2a114214a1a3432a7092f9cc8b9f5747
majovec <jakub.mai97@gmail.com>
2017-06-05 01:17:22 +0200

    Update MovePlayerPacket.php
    Fix this stupid bug [22:53:27] [Server thread/CRITICAL]: Could not tick level "GamesLobby": Undefined class constant 'MODE_ROTATION'
    [22:53:27] [Server thread/CRITICAL]: Could not tick level "GamesLobby": Undefined class constant 'MODE_ROTATION'

--- src/pocketmine/network/protocol/MovePlayerPacket.php
+++ src/pocketmine/network/protocol/MovePlayerPacket.php
@@ -24,13 +24,13 @@ namespace pocketmine\network\protocol;
 #include <rules/DataPacket.h>
 
 
-class MovePlayerPacket extends DataPacket {
+class MovePlayerPacket extends DataPacket{
 	const NETWORK_ID = Info::MOVE_PLAYER_PACKET;
 
 	const MODE_NORMAL = 0;
 	const MODE_RESET = 1;
-	const MODE_POTATION = 2;
-	const MODE_PITCH = 3; //facepalm Mojang
+	const MODE_TELEPORT = 2;
+	const MODE_PITCH = 3; 
 
 	public $eid;
 	public $x;
@@ -41,7 +41,7 @@ class MovePlayerPacket extends DataPacket {
 	public $pitch;
 	public $mode = self::MODE_NORMAL;
 	public $onGround;
-	public $ridingEid;
+	public $ridingEid = 0;
 	public $int1 = 0;
 	public $int2 = 0;
 
@@ -54,7 +54,7 @@ class MovePlayerPacket extends DataPacket {
 		$this->mode = $this->getByte();
 		$this->onGround = $this->getBool();
 		$this->ridingEid = $this->getEntityRuntimeId();
-		if($this->mode === MovePlayerPacket::MODE_ROTATION){
+		if($this->mode === MovePlayerPacket::MODE_TELEPORT){
 			$this->int1 = $this->getLInt();
 			$this->int2 = $this->getLInt();
 		}
@@ -66,14 +66,18 @@ class MovePlayerPacket extends DataPacket {
 		$this->putVector3f($this->x, $this->y, $this->z);
 		$this->putLFloat($this->pitch);
 		$this->putLFloat($this->yaw);
-		$this->putLFloat($this->bodyYaw); //TODO
+		$this->putLFloat($this->bodyYaw); 
 		$this->putByte($this->mode);
 		$this->putBool($this->onGround);
 		$this->putEntityRuntimeId($this->ridingEid);
-		if($this->mode === MovePlayerPacket::MODE_ROTATION){
+		if($this->mode === MovePlayerPacket::MODE_TELEPORT){
 			$this->putLInt($this->int1);
 			$this->putLInt($this->int2);
 		}
+	}
+
+	public function handle(NetworkSession $session) : bool{
+		return $session->handleMovePlayer($this);
 	}
 
 }
